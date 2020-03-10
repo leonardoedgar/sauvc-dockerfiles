50fad6eb7d518c726370ac7f85f456af7c918dfd
quan <quanng95@gmail.com>
2020-03-10 11:51:23 +0800

    bugfix with serial connection inside docker

--- run_serial_node.sh
+++ run_serial_node.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [$MACHINE = LAPTOP]
+if [ $MACHINE == LAPTOP ]
 then
     PORT=/dev/ttyUSB0
-elif [$MACHINE = ROBOT]
+elif [ $MACHINE == ROBOT ]
 then
 	PORT=/dev/ttyACM0
 fi
-/ros_entrypoint.sh rosrun rosserial_python serial_node.py $PORT
+/ros_entrypoint.sh rosrun rosserial_python serial_node.py _port:=$PORT
